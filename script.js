c9afb85d63cb7146aa5b9a324d0e5953e37b178f
Nate Shuster <shusternathaniel@gmail.com>
2023-02-13 17:53:18 -0500

    Build displayBooks function and begin restyle

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-let myLibrary = [];
+let myLibrary = [
+  {
+    title: "No Longer Human",
+    author: "Osamu Dazai",
+    read: "Yes"
+  }
+];
 
-function Book() {
-  // the constructor...
+function Book(title, author, read) {
+  this.title = title
+  this.author = author
+  this.read = "Yes"
+  
+  if (read === "No") {
+    this.read = "No"
+  }
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(input) {
+  
 }
+
+function displayBooks() {
+  const cardDiv = document.querySelector(".cards");
+
+  myLibrary.forEach(book => {
+    // create new card
+    const card = document.createElement("div")
+
+    // create data elements
+    const title = document.createElement("h3")
+    title.textContent += book.title
+    const author = document.createElement("p")
+    author.textContent += book.author
+    const read = document.createElement("p")
+    read.textContent += book.read
+
+    // add data elements to card
+    card.appendChild(title)
+    card.appendChild(author)
+    card.appendChild(read)
+
+    // add card to card div
+    cardDiv.appendChild(card)
+  });
+}
+
+displayBooks();
