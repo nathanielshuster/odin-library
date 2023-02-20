7d03c98be47ef9123149d9a26a0600a25003e168
Nate Shuster <shusternathaniel@gmail.com>
2023-02-20 17:37:58 -0500

    Build status update function

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ function displayBook(book) {
   // create new card
   const card = document.createElement("div")
   card.classList.add("card")
+  card.dataset.index = myLibrary.indexOf(book)
 
   // create data elements
   const title = document.createElement("h3")
@@ -41,16 +42,71 @@ function displayBook(book) {
   title.textContent += book.title
   const author = document.createElement("p")
   author.classList.add("author")
-  author.textContent += book.author
-  const status = document.createElement("p")
-  status.classList.add("status")
-  status.textContent += book.read
+  author.textContent += `By: ${book.author}`
 
-  // add data elements to card
-  card.appendChild(title)
-  card.appendChild(author)
-  card.appendChild(status)
+  // create data element div
+  const dataDiv = document.createElement("div")
+  dataDiv.classList.add("data-div")
+
+  //add data to data div
+  dataDiv.appendChild(title)
+  dataDiv.appendChild(author)
+
+  // create card buttons
+  const readButton = createReadButton(book)
+
+  const removeButton = document.createElement("button")
+  removeButton.textContent = "Delete"
+  removeButton.classList.add("delete-button")
+  removeButton.onclick = removeBook
+
+  // create card button div
+  const buttonDiv = document.createElement("div")
+  buttonDiv.classList.add("button-div")
+
+  // add buttons to button div
+  buttonDiv.appendChild(readButton)
+  buttonDiv.appendChild(removeButton)
+
+  // add elements to card
+  card.appendChild(dataDiv)
+  card.appendChild(buttonDiv)
 
   // add card to card div
   container.appendChild(card)
+}
+
+function removeBook(e) {
+  let index = e.target.parentElement.parentElement.dataset.index
+  myLibrary.splice(Number(index), 1)
+  let card = document.querySelector(`[data-index = '${index}']`)
+  card.remove()
+}
+
+function updateStatus(e) {
+  let index = e.target.parentElement.parentElement.dataset.index
+  let book = myLibrary[Number(index)]
+
+  let button = e.target
+  if (e.target.textContent === "Read") {
+    button.textContent = "Unread"
+    book.read = "Unread"
+  } else {
+    button.textContent = "Read"
+    book.read = "Read"
+  }
+}
+
+function createReadButton(book) {
+  const button = document.createElement("button")
+  button.classList.add("read-button")
+  button.onclick = updateStatus
+
+  if (book.read === "Read") {
+    button.textContent = "Read"
+    return button
+  } else {
+    button.textContent = "Unread"
+    return button
+  }
 }
